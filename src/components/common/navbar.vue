7d895d3d54a7db412c1ddd1dc9c7a986695e7c89
zgy <zgyandy@126.com>
2018-07-30 11:47:51 +0800

    顶部导航修改

--- src/components/common/navbar.vue
+++ src/components/common/navbar.vue
@@ -1,108 +1,108 @@
 <template>
   <div class="navbar">
-    <div class="navBox layout">
-      <p class="logo">
-        <router-link to="/home" tag="span" class="iconfont icon-move"></router-link>
-      </p>
-      <ul class="navList">
-        <li>
-          <router-link to="/home">首页</router-link>
-        </li>
-        <li>
-          <router-link to="/">页面1</router-link>
-        </li>
-        <li>
-          <router-link to="/">页面2</router-link>
-        </li>
-        <li>
-          <router-link to="/">页面3</router-link>
-        </li>
-      </ul>
-      <ul class="userIcon">
-        <li v-if="$store.state.isLogin === 1">
-          <router-link to="/user">
-            <img class="uerPhone" src="../../assets/logo.png" alt="">
-          </router-link>
-        </li>
-        <li v-if="$store.state.isLogin === 0" @click.self="reg" class="register">注册</li>
-        <li v-if="$store.state.isLogin === 0" @click.self="log" class="login">登录</li>
-      </ul>
-    </div>
+    <ul class="layout header">
+      <li class="logo">
+        <p><img src="../../assets/img/logo.png" alt=""></p>
+        <h3>海同科技CRM管理系统</h3>
+      </li>
+      <li class="search">
+        <el-input
+          placeholder="请输入客户名/手机号查询"
+          v-model="search"
+          suffix-icon="el-icon-search"
+          size="medium"
+        >
+        <el-button slot="append">搜索</el-button>
+        </el-input>
+      </li>
+      <li class="userMsg">
+        <p>
+          <span>您好：<em class="userName">{{userName}}</em><span>，欢迎登陆！上次登录时间：<em class="time">{{loginTime}}</em></span></span>
+          <span class="loginOut">退出</span>
+        </p>
+        <dl>
+          <dd><span>今日通话时长：<em class="callTime">{{callTime}}</em></span></dd>
+          <dd><span>通话次数：<em class="callNum">{{callNum}}</em></span></dd>
+          <dd><span><em class="el-icon-message"></em><em class="msgNum">{{msgNum}}</em>条未处理</span></dd>
+        </dl>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    reg () {
-      this.$emit('ragister')
-    },
-    log () {
-      this.$emit('signIn')
+  data () {
+    return {
+      search: '',
+      userName: '小明',
+      loginTime: '2018-06-12 16:04:03',
+      callTime: '00:00:00',
+      callNum: 12,
+      msgNum: 3
     }
   }
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
   .navbar {
-    height: 60px;
+    height: 110 - 50px;
+    padding: 25px 0;
     min-width: 1200px;
-    background-color: #2b313a;
-    color: #fff;
-    .navBox {
+    background-color: #428adf;
+    .header {
       height: 60px;
       display: flex;
       display: -webkit-flex;
       justify-content: space-between;
       align-items: center;
-      .logo {
+      li {
         height: 60px;
-        width: 60px;
-        line-height: 60px;
-      }
-      .navList {
-        height: 60px;
-        display: flex;
-        display:-webkit-flex;
-        align-items: center;
-        flex: 1;
-        li {
-          height: 40px;
-          line-height: 40px;
+        color: #fff;
+        h3 {
           font-size: 20px;
-          padding: 0 20px;
+        }
+        h4 {
+          font-size: 16px;
         }
       }
-      .userIcon {
+      .search {
         height: 40px;
+        .el-input__inner {
+          width: 320px;
+          height: 40px;
+        }
+        .el-icon-search {
+          font-size: 20px;
+        }
+      }
+      .userMsg {
+        min-width: 380px;
         display: flex;
         display: -webkit-flex;
-        align-items: center;
-        li {
+        justify-content: space-between;
+        flex-direction: column;
+        p {
+          height: 16px;
+          line-height: 16px;
+          .loginOut::before {
+            content: '|';
+            padding: 0 10px;
+          }
+        }
+        dl {
           height: 30px;
           line-height: 30px;
-          text-align: center;
-          border-radius: 5px;
-          cursor: pointer;
-        }
-        .uerPhone {
-          height: 30px;
-          width: 30px;
-          border-radius: 50%;
-          background: #fff;
-          vertical-align: bottom;
-        }
-        .register {
-          width: 50px;
-          margin-right: 10px;
-          background: yellow;
-          color: green;
-        }
-        .login {
-          width: 50px;
-          background: green;
-          // color: yellow;
+          font-size: 15px;
+          display: flex;
+          display: -webkit-flex;
+          justify-content: space-between;
+          align-items: center;
+          .el-icon-message {
+            font-size: 18px;
+            cursor: pointer;
+          }
         }
       }
     }
